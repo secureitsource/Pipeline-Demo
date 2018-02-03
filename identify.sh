168601b42edd0a5b7ba1d2ce6160c651d6f2c1a8
Pete Greenwood <ptr.grnwd@gmail.com>
2018-02-03 16:45:33 -0500

    Update identify.sh

--- identify.sh
+++ identify.sh
@@ -8,10 +8,10 @@ function main() {
 function fetch_machine_identity() {
 
   ########### CHANGE VARIABLES BELOW ############
-  local baseurl='https://conjurucd'
+  local baseurl='https://conjur_master'
   local hostid='jenkins/masters/master01'
   local token=$(cat ~/src/hftoken.txt | tr -d "\n")
-  local cert='~/conjur-secureitsource.pem'
+  local cert='/root/conjur-dev.pem'
   ###############################################
 
   echo 'Fetching machine identity from Conjur'
